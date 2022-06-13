c135b07bec1da137826acc33bfd9ea76d0480095
kakuchrome <73450989+kakuchrome@users.noreply.github.com>
2022-06-14 08:38:19 +0900

    Update timer.py
    fix

--- timer.py
+++ timer.py
@@ -14,11 +14,13 @@ if __name__ == "__main__":
   client = udp_client.SimpleUDPClient(args.ip, args.port)
   
 dt_now =  datetime.datetime.now()
+base = datetime.time(7, 0, 0)
+now = dt_now.time()
 
 if __name__ == '__main__':
     while True:
         # when realtime is beyond 7am then set timer osc parameter to true
-        if dt_now.hour < 7:
+        if base > now :
             client.send_message("/avatar/parameters/timer", 0)
             print("timer off ")
         else:
